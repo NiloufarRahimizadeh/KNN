b21748fd4ff2d73e2437ceee9522e47c5f3bf52b
poulstar <info@poulstar.com>
2021-05-25 17:35:11 +0430

    second commit

--- test01.py
+++ test01.py
@@ -5,3 +5,8 @@ from matplotlib.ticker import NullFormatter
 import matplotlib.ticker as ticker
 import pandas as pd 
 from sklearn import preprocessing
+
+df = pd.read_csv('teleCust1000t.csv')
+df.head()
+# df['custcat'].value_counts()
+df.hist(column='income')
